bf66239d25e4c25bbe66860dd2872b40d4d53d62
史东杰 <shidongjie@shidongjies-Mac.local>
2014-06-23 19:11:26 +0800

    Initial Commit

---
+++ ShangShang/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ShangShang
+//
+//  Created by 史东杰 on 14-6-23.
+//  Copyright (c) 2014年 aopai.ios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ShangShang/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ShangShang
+//
+//  Created by 史东杰 on 14-6-23.
+//  Copyright (c) 2014年 aopai.ios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
