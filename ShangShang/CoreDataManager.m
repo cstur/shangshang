47193ef61afe409af4eb85f757a5dbeb57ae7808
sdongjie <sdongjie@126.com>
2015-05-17 19:57:25 +0800

    remove errors

--- ShangShang/CoreDataManager.m
+++ ShangShang/CoreDataManager.m
@@ -1,204 +1,205 @@
+////
+////  CoreDataManager.m
+////  ShangShang
+////
+////  Created by 史东杰 on 14/12/29.
+////  Copyright (c) 2014年 aopai.ios. All rights reserved.
+////
 //
-//  CoreDataManager.m
-//  ShangShang
 //
-//  Created by 史东杰 on 14/12/29.
-//  Copyright (c) 2014年 aopai.ios. All rights reserved.
+//#import "CoreDataManager.h"
 //
-
-
-#import "CoreDataManager.h"
-
-@implementation CoreDataManager
-
-@synthesize managedObjectContext = _managedObjectContext;
-//@synthesize managedObjectModel = _managedObjectModel;
-//@synthesize persistentStoreCoordinator = _persistentStoreCoordinator;
-
-- (void)saveContext
-{
-    NSError *error = nil;
-    NSManagedObjectContext *managedObjectContext = self.managedObjectContext;
-    if (managedObjectContext != nil) {
-        if ([managedObjectContext hasChanges] && ![managedObjectContext save:&error]) {
-            NSLog(@"Unresolved error %@, %@", error, [error userInfo]);
-        }
-    }
-}
-
-#pragma mark - Core Data stack
-
-// Returns the managed object context for the application.
-// If the context doesn't already exist, it is created and bound to the persistent store coordinator for the application.
-- (NSManagedObjectContext *)managedObjectContext
-{
-    if(managedObjectContext == nil){
-        NSURL *url = [[[NSFileManager defaultManager] URLsForDirectory:NSDocumentDirectory
-                                                             inDomains:NSUserDomainMask] lastObject];
-        NSURL *storeDatabaseURL = [url URLByAppendingPathComponent:@"Smurf.sqlite"];
-        // 设置SQLite 数据库存储路径 /ShoppingCart.sqlite
-        NSError *error = nil;
-        
-        //根据被管理对象模型创建NSPersistentStoreCoordinator 对象实例
-        NSPersistentStoreCoordinator *persistentStoreCoordinator =
-        [[NSPersistentStoreCoordinator alloc]
-         initWithManagedObjectModel:[NSManagedObjectModel mergedModelFromBundles:nil]];
-        
-        //根据指定的存储类型和路径，创建一个新的持久化存储（Persistent Store）
-        if(![persistentStoreCoordinator addPersistentStoreWithType:NSSQLiteStoreType
-                                                     configuration:nil
-                                                               URL:storeDatabaseURL options:nil error:&error])
-        {
-            NSLog(@"Error while loading persistent store ...%@", error);
-        }
-        
-        managedObjectContext = [[NSManagedObjectContext alloc] init];
-        
-        //设置当前被管理对象上下文的持久化存储协调器
-        [managedObjectContext setPersistentStoreCoordinator:persistentStoreCoordinator];
-    }
-    
-    // 返回初始化的被管理对象上下文实例
-    return managedObjectContext;
-}
-
-// Returns the managed object model for the application.
-// If the model doesn't already exist, it is created from the application's model.
-/*
-- (NSManagedObjectModel *)managedObjectModel
-{
-    if (_managedObjectModel != nil) {
-        return _managedObjectModel;
-    }
-    NSURL *modelURL = [[NSBundle mainBundle] URLForResource:@"NewsModel" withExtension:@"momd"];
-    _managedObjectModel = [[NSManagedObjectModel alloc] initWithContentsOfURL:modelURL];
-    return _managedObjectModel;
-}*/
-
-// Returns the persistent store coordinator for the application.
-// If the coordinator doesn't already exist, it is created and the application's store added to it.
-- (NSPersistentStoreCoordinator *)persistentStoreCoordinator
-{
-    if (_persistentStoreCoordinator != nil) {
-        return _persistentStoreCoordinator;
-    }
-    
-    NSURL *storeURL = [[self applicationDocumentsDirectory] URLByAppendingPathComponent:@"NewsModel.sqlite"];
-    
-    NSError *error = nil;
-    _persistentStoreCoordinator = [[NSPersistentStoreCoordinator alloc] initWithManagedObjectModel:[NSManagedObjectModel mergedModelFromBundles:nil]];
-    if (![_persistentStoreCoordinator addPersistentStoreWithType:NSSQLiteStoreType configuration:nil URL:storeURL options:nil error:&error]) {
-        NSLog(@"Unresolved error %@, %@", error, [error userInfo]);
-        abort();
-    }
-    
-    return _persistentStoreCoordinator;
-}
-
-#pragma mark - Application's Documents directory
-
-// Returns the URL to the application's Documents directory.获取Documents路径
-- (NSURL *)applicationDocumentsDirectory
-{
-    return [[[NSFileManager defaultManager] URLsForDirectory:NSDocumentDirectory inDomains:NSUserDomainMask] lastObject];
-}
-
-//插入数据
-- (void)insertCoreData:(NSMutableArray*)dataArray
-{
-    NSManagedObjectContext *context = [self managedObjectContext];
-    for (News *info in dataArray) {
-        News *newsInfo = [NSEntityDescription insertNewObjectForEntityForName:TableName inManagedObjectContext:context];
-        newsInfo.newsid = info.newsid;
-        newsInfo.title = info.title;
-        newsInfo.imgurl = info.imgurl;
-        newsInfo.descr = info.descr;
-        newsInfo.islook = info.islook;
-        
-        NSError *error;
-        if(![context save:&error])
-        {
-            NSLog(@"不能保存：%@",[error localizedDescription]);
-        }
-    }
-}
-
-//查询
-- (NSMutableArray*)selectData:(int)pageSize andOffset:(int)currentPage
-{
-    NSManagedObjectContext *context = [self managedObjectContext];
-    
-    // 限定查询结果的数量
-    //setFetchLimit
-    // 查询的偏移量
-    //setFetchOffset
-    
-    NSFetchRequest *fetchRequest = [[NSFetchRequest alloc] init];
-    
-    [fetchRequest setFetchLimit:pageSize];
-    [fetchRequest setFetchOffset:currentPage];
-    
-    NSEntityDescription *entity = [NSEntityDescription entityForName:TableName inManagedObjectContext:context];
-    [fetchRequest setEntity:entity];
-    NSError *error;
-    NSArray *fetchedObjects = [context executeFetchRequest:fetchRequest error:&error];
-    NSMutableArray *resultArray = [NSMutableArray array];
-    
-    for (News *info in fetchedObjects) {
-        NSLog(@"id:%@", info.newsid);
-        NSLog(@"title:%@", info.title);
-        [resultArray addObject:info];
-    }
-    return resultArray;
-}
-
-//删除
--(void)deleteData
-{
-    NSManagedObjectContext *context = [self managedObjectContext];
-    NSEntityDescription *entity = [NSEntityDescription entityForName:TableName inManagedObjectContext:context];
-    
-    NSFetchRequest *request = [[NSFetchRequest alloc] init];
-    [request setIncludesPropertyValues:NO];
-    [request setEntity:entity];
-    NSError *error = nil;
-    NSArray *datas = [context executeFetchRequest:request error:&error];
-    if (!error && datas && [datas count])
-    {
-        for (NSManagedObject *obj in datas)
-        {
-            [context deleteObject:obj];
-        }
-        if (![context save:&error])
-        {
-            NSLog(@"error:%@",error);
-        }
-    }
-}
-//更新
-- (void)updateData:(NSString*)newsId  withIsLook:(NSString*)islook
-{
-    NSManagedObjectContext *context = [self managedObjectContext];
-    
-    NSPredicate *predicate = [NSPredicate
-                              predicateWithFormat:@"newsid like[cd] %@",newsId];
-    
-    //首先你需要建立一个request
-    NSFetchRequest * request = [[NSFetchRequest alloc] init];
-    [request setEntity:[NSEntityDescription entityForName:TableName inManagedObjectContext:context]];
-    [request setPredicate:predicate];//这里相当于sqlite中的查询条件，具体格式参考苹果文档
-    
-    //https://developer.apple.com/library/mac/#documentation/Cocoa/Conceptual/Predicates/Articles/pCreating.html
-    NSError *error = nil;
-    NSArray *result = [context executeFetchRequest:request error:&error];//这里获取到的是一个数组，你需要取出你要更新的那个obj
-    for (News *info in result) {
-        info.islook = islook;
-    }
-    
-    //保存
-    if ([context save:&error]) {
-        //更新成功
-        NSLog(@"更新成功");
-    }
-}
-@end
+//@implementation CoreDataManager
+//
+//@synthesize managedObjectContext = _managedObjectContext;
+////@synthesize managedObjectModel = _managedObjectModel;
+////@synthesize persistentStoreCoordinator = _persistentStoreCoordinator;
+//
+//- (void)saveContext
+//{
+//    NSError *error = nil;
+//    NSManagedObjectContext *managedObjectContext = self.managedObjectContext;
+//    if (managedObjectContext != nil) {
+//        if ([managedObjectContext hasChanges] && ![managedObjectContext save:&error]) {
+//            NSLog(@"Unresolved error %@, %@", error, [error userInfo]);
+//        }
+//    }
+//}
+//
+//#pragma mark - Core Data stack
+//
+//// Returns the managed object context for the application.
+//// If the context doesn't already exist, it is created and bound to the persistent store coordinator for the application.
+//- (NSManagedObjectContext *)managedObjectContext
+//{
+//    if(managedObjectContext == nil){
+//        NSURL *url = [[[NSFileManager defaultManager] URLsForDirectory:NSDocumentDirectory
+//                                                             inDomains:NSUserDomainMask] lastObject];
+//        NSURL *storeDatabaseURL = [url URLByAppendingPathComponent:@"Smurf.sqlite"];
+//        // 设置SQLite 数据库存储路径 /ShoppingCart.sqlite
+//        NSError *error = nil;
+//        
+//        //根据被管理对象模型创建NSPersistentStoreCoordinator 对象实例
+//        NSPersistentStoreCoordinator *persistentStoreCoordinator =
+//        [[NSPersistentStoreCoordinator alloc]
+//         initWithManagedObjectModel:[NSManagedObjectModel mergedModelFromBundles:nil]];
+//        
+//        //根据指定的存储类型和路径，创建一个新的持久化存储（Persistent Store）
+//        if(![persistentStoreCoordinator addPersistentStoreWithType:NSSQLiteStoreType
+//                                                     configuration:nil
+//                                                               URL:storeDatabaseURL options:nil error:&error])
+//        {
+//            NSLog(@"Error while loading persistent store ...%@", error);
+//        }
+//        
+//        managedObjectContext = [[NSManagedObjectContext alloc] init];
+//        
+//        //设置当前被管理对象上下文的持久化存储协调器
+//        [managedObjectContext setPersistentStoreCoordinator:persistentStoreCoordinator];
+//    }
+//    
+//    // 返回初始化的被管理对象上下文实例
+//    return managedObjectContext;
+//}
+//
+//// Returns the managed object model for the application.
+//// If the model doesn't already exist, it is created from the application's model.
+///*
+//- (NSManagedObjectModel *)managedObjectModel
+//{
+//    if (_managedObjectModel != nil) {
+//        return _managedObjectModel;
+//    }
+//    NSURL *modelURL = [[NSBundle mainBundle] URLForResource:@"NewsModel" withExtension:@"momd"];
+//    _managedObjectModel = [[NSManagedObjectModel alloc] initWithContentsOfURL:modelURL];
+//    return _managedObjectModel;
+//}*/
+//
+//// Returns the persistent store coordinator for the application.
+//// If the coordinator doesn't already exist, it is created and the application's store added to it.
+//- (NSPersistentStoreCoordinator *)persistentStoreCoordinator
+//{
+//    if (_persistentStoreCoordinator != nil) {
+//        return _persistentStoreCoordinator;
+//    }
+//    
+//    NSURL *storeURL = [[self applicationDocumentsDirectory] URLByAppendingPathComponent:@"NewsModel.sqlite"];
+//    
+//    NSError *error = nil;
+//    _persistentStoreCoordinator = [[NSPersistentStoreCoordinator alloc] initWithManagedObjectModel:[NSManagedObjectModel mergedModelFromBundles:nil]];
+//    if (![_persistentStoreCoordinator addPersistentStoreWithType:NSSQLiteStoreType configuration:nil URL:storeURL options:nil error:&error]) {
+//        NSLog(@"Unresolved error %@, %@", error, [error userInfo]);
+//        abort();
+//    }
+//    
+//    return _persistentStoreCoordinator;
+//}
+//
+//#pragma mark - Application's Documents directory
+//
+//// Returns the URL to the application's Documents directory.获取Documents路径
+//- (NSURL *)applicationDocumentsDirectory
+//{
+//    return [[[NSFileManager defaultManager] URLsForDirectory:NSDocumentDirectory inDomains:NSUserDomainMask] lastObject];
+//}
+//
+////插入数据
+//- (void)insertCoreData:(NSMutableArray*)dataArray
+//{
+//    NSManagedObjectContext *context = [self managedObjectContext];
+//    for (News *info in dataArray) {
+//        News *newsInfo = [NSEntityDescription insertNewObjectForEntityForName:TableName inManagedObjectContext:context];
+//        newsInfo.newsid = info.newsid;
+//        newsInfo.title = info.title;
+//        newsInfo.imgurl = info.imgurl;
+//        newsInfo.descr = info.descr;
+//        newsInfo.islook = info.islook;
+//        
+//        NSError *error;
+//        if(![context save:&error])
+//        {
+//            NSLog(@"不能保存：%@",[error localizedDescription]);
+//        }
+//    }
+//}
+//
+////查询
+//- (NSMutableArray*)selectData:(int)pageSize andOffset:(int)currentPage
+//{
+//    NSManagedObjectContext *context = [self managedObjectContext];
+//    
+//    // 限定查询结果的数量
+//    //setFetchLimit
+//    // 查询的偏移量
+//    //setFetchOffset
+//    
+//    NSFetchRequest *fetchRequest = [[NSFetchRequest alloc] init];
+//    
+//    [fetchRequest setFetchLimit:pageSize];
+//    [fetchRequest setFetchOffset:currentPage];
+//    
+//    NSEntityDescription *entity = [NSEntityDescription entityForName:TableName inManagedObjectContext:context];
+//    [fetchRequest setEntity:entity];
+//    NSError *error;
+//    NSArray *fetchedObjects = [context executeFetchRequest:fetchRequest error:&error];
+//    NSMutableArray *resultArray = [NSMutableArray array];
+//    
+//    for (News *info in fetchedObjects) {
+//        NSLog(@"id:%@", info.newsid);
+//        NSLog(@"title:%@", info.title);
+//        [resultArray addObject:info];
+//    }
+//    return resultArray;
+//}
+//
+////删除
+//-(void)deleteData
+//{
+//    NSManagedObjectContext *context = [self managedObjectContext];
+//    NSEntityDescription *entity = [NSEntityDescription entityForName:TableName inManagedObjectContext:context];
+//    
+//    NSFetchRequest *request = [[NSFetchRequest alloc] init];
+//    [request setIncludesPropertyValues:NO];
+//    [request setEntity:entity];
+//    NSError *error = nil;
+//    NSArray *datas = [context executeFetchRequest:request error:&error];
+//    if (!error && datas && [datas count])
+//    {
+//        for (NSManagedObject *obj in datas)
+//        {
+//            [context deleteObject:obj];
+//        }
+//        if (![context save:&error])
+//        {
+//            NSLog(@"error:%@",error);
+//        }
+//    }
+//}
+////更新
+//- (void)updateData:(NSString*)newsId  withIsLook:(NSString*)islook
+//{
+//    NSManagedObjectContext *context = [self managedObjectContext];
+//    
+//    NSPredicate *predicate = [NSPredicate
+//                              predicateWithFormat:@"newsid like[cd] %@",newsId];
+//    
+//    //首先你需要建立一个request
+//    NSFetchRequest * request = [[NSFetchRequest alloc] init];
+//    [request setEntity:[NSEntityDescription entityForName:TableName inManagedObjectContext:context]];
+//    [request setPredicate:predicate];//这里相当于sqlite中的查询条件，具体格式参考苹果文档
+//    
+//    //https://developer.apple.com/library/mac/#documentation/Cocoa/Conceptual/Predicates/Articles/pCreating.html
+//    NSError *error = nil;
+//    NSArray *result = [context executeFetchRequest:request error:&error];//这里获取到的是一个数组，你需要取出你要更新的那个obj
+//    for (News *info in result) {
+//        info.islook = islook;
+//    }
+//    
+//    //保存
+//    if ([context save:&error]) {
+//        //更新成功
+//        NSLog(@"更新成功");
+//    }
+//}
+//
+//@end
